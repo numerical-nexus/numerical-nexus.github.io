1b4e809e64dc36714b4cd796122013c25585cb22
Justin Burzachiello <justinburzachiello@gmail.com>
2025-03-15 16:49:38 -0700

    attempt1: iframe site with marimo

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const algorithmDropdown = document.getElementById("algorithm-dropdown");
 
     algorithmDropdown.addEventListener("change", function () {
-        frame.src = this.value || "about:blank"; // Load the selected algorithm or reset if none
+        frame.src = this.value || "about:blank"; // Load the selected notebook or reset if none
     });
 });
