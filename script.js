b27b1cf13829366a471148e338b5ac075eb81ed4
Justin Burzachiello <justinburzachiello@gmail.com>
2025-03-15 16:21:01 -0700

    initial HTML, CSS, JS

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const frame = document.getElementById("tutorial-frame");
+    const algorithmDropdown = document.getElementById("algorithm-dropdown");
+
+    algorithmDropdown.addEventListener("change", function () {
+        frame.src = this.value || "about:blank"; // Load the selected algorithm or reset if none
+    });
+});
