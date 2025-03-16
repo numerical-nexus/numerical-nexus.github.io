6bed4a8e83a4521b951d69c7e4caf27497027379
Justin Burzachiello <justinburzachiello@gmail.com>
2025-03-15 17:45:17 -0700

    removed files

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const frame = document.getElementById("tutorial-frame");
-    const algorithmDropdown = document.getElementById("algorithm-dropdown");
-
-    algorithmDropdown.addEventListener("change", function () {
-        frame.src = this.value || "about:blank"; // Load the selected notebook or reset if none
-    });
-});
